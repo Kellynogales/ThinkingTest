b5399aaa9a622d49def58845e17d498c99e3a1b6
HunterDLuis <lvv.umss@gmail.com>
2021-09-18 23:20:13 -0400

    Add model contact and user

--- src/test/java/BaseApi.java
+++ src/test/java/BaseApi.java
@@ -1,3 +1,37 @@
-public class BaseApi {
+//import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.Filter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.BeforeClass;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseApi {
+    private static final Logger logger = LogManager.getLogger(ThinkingTest.class);
+
+    @BeforeClass
+    public static void setup(){
+        logger.info("Start configuration");
+        RestAssured.requestSpecification = defaultRequestSpecification();
+        logger.info("Successfully configuration");
+    }
+
+    public static RequestSpecification defaultRequestSpecification(){
+
+        List<Filter> filters = new ArrayList<>();
+        filters.add(new RequestLoggingFilter());
+        filters.add(new ResponseLoggingFilter());
+//        filters.add(new AllureRestAssured());
+
+        return new RequestSpecBuilder().setBaseUri("https://thinking-tester-contact-list.herokuapp.com")
+                .addFilters(filters)
+                .setContentType(ContentType.JSON).build();
+    }
 }
--- src/test/java/ThinkingTest.java
+++ src/test/java/ThinkingTest.java
@@ -1,76 +1,69 @@
-import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import model.contact.PostContactRequest;
+import model.user.PostUserRequest;
 import org.apache.http.HttpStatus;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static io.restassured.path.json.JsonPath.from;
+import static org.hamcrest.Matchers.*;
 
-public class ThinkingTest {
-    @BeforeClass
-    public void setup(){
-        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
-    }
+public class ThinkingTest extends BaseApi {
     /*POST Add User*/
     @Test
     public void addUserSuccessfullyTest(){
-        given()
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"firstName\": \"Luis20\",\n" +
-                        "    \"lastName\": \"Villa20\",\n" +
-                        "    \"email\": \"luis20@gmail.com\",\n" +
-                        "    \"password\": \"myPassword\"\n" +
-                        "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users")
-                .then()
-                .statusCode(HttpStatus.SC_CREATED);
 
-    }
-    @Test
-    public void responseCodeBadRequestWhenAddInvalidUserTest(){
-        given()
-                .contentType(ContentType.JSON)
-                .log().all()
-                .body("{\n" +
-                        "    \"firstName\": \"\",\n" +
-                        "    \"lastName\": \"\",\n" +
-                        "    \"email\": \"\",\n" +
-                        "    \"password\": \"\"\n" +
-                        "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .body("message", notNullValue());
+        PostUserRequest userRequest = new PostUserRequest("Luis33", "Villa33", "luis33@gmail.com", "myPassword");
 
+        Response responseUser = genericMethodPostAddUser(userRequest, "users");
+
+        int statusCode = responseUser.getStatusCode();
+        String body = responseUser.getBody().asString();
+
+        assertThat(statusCode, equalTo(HttpStatus.SC_CREATED));
+
+        String tokenUser = from(body).get("token");
+        assertThat(tokenUser, notNullValue());
     }
 
     @Test
-    public void getResponseWhenAddUserSuccessfullyTest(){
-        String userFirstName = given()
-                .contentType(ContentType.JSON)
-                .log().all()
-                .body("{\n" +
-                        "    \"firstName\": \"Luis21\",\n" +
-                        "    \"lastName\": \"Villa21\",\n" +
-                        "    \"email\": \"luis21@gmail.com\",\n" +
-                        "    \"password\": \"myPassword\"\n" +
-                        "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_CREATED)
-                .body("token", notNullValue())
-                .body("user._id", notNullValue())
-                .extract().jsonPath().getString("firstName");
+    public void responseCodeBadRequestInAddUserWhenSendMandatoryFieldsEmptyTest(){
 
-        assertThat(userFirstName, equalTo("Luis21"));
+        PostUserRequest userRequest = new PostUserRequest("", "", "", "");
+
+        Response responseUser = genericMethodPostAddUser(userRequest, "users");
+
+        int statusCode = responseUser.getStatusCode();
+        String body = responseUser.getBody().asString();
+
+        assertThat(statusCode, equalTo(HttpStatus.SC_BAD_REQUEST));
+
+        String message = from(body).get("message");
+        assertThat(message, notNullValue());
+        assertThat(message, equalTo("User validation failed: firstName: Path `firstName` is required., lastName: Path `lastName` is required., email: Email is invalid, password: Path `password` is required."));
+    }
+
+    @Test
+    public void getFirsNameWhenAddUserSuccessfullyTest(){
+
+        PostUserRequest userRequest = new PostUserRequest("Luis34", "Villa34", "luis34@gmail.com", "myPassword");
+
+        Response responseUser = genericMethodPostAddUser(userRequest, "users");
+
+        int statusCode = responseUser.getStatusCode();
+        String body = responseUser.getBody().asString();
+
+        assertThat(statusCode, equalTo(HttpStatus.SC_CREATED));
+
+        String token = from(body).get("token");
+        String userId = from(body).get("user._id");
+        String userFirstName = from(body).get("user.firstName");
+
+        assertThat(token, notNullValue());
+        assertThat(userId, notNullValue());
+        assertThat(userFirstName, equalTo("Luis34"));
     }
     /*POST Log User*/
     @Test
@@ -83,20 +76,17 @@ public class ThinkingTest {
                         "    \"email\": \"luis20@gmail.com\",\n" +
                         "    \"password\": \"myPassword\"\n" +
                         "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/login")
+                .post("users/login")
                 .then()
+                .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body("token", notNullValue())
                 .extract().jsonPath().getString("token");
 
-        given()
-                .auth()
-                .oauth2(userToken)
-                .log().all()
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/logout")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_OK);
+        Response responseUser = genericMethodPostLogOutUser(userToken, "users/logout");
+
+        int statusCode = responseUser.getStatusCode();
+        assertThat(statusCode, equalTo(HttpStatus.SC_OK));
     }
 
     @Test
@@ -109,41 +99,36 @@ public class ThinkingTest {
                         "    \"email\": \"luis20@gmail.com\",\n" +
                         "    \"password\": \"myPassword\"\n" +
                         "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/login")
+                .post("users/login")
                 .then()
+                .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body("token", notNullValue())
                 .extract().jsonPath().getString("token");
+        Response responseUser = genericMethodPostLogOutUser(userToken+"a", "users/logout");
 
-        given()
-                .auth()
-                .oauth2(userToken+"a")
-                .log().all()
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/logout")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED);
+        int statusCode = responseUser.getStatusCode();
+        assertThat(statusCode, equalTo(HttpStatus.SC_UNAUTHORIZED));
     }
 
     @Test
     public void verifyIfReceiveErrorMessageWhenUseTokenEmptyTest(){
-        String errorMessage = given()
-                .auth()
-                .oauth2("")
-                .log().all()
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/logout")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED)
-                .body("error", notNullValue())
-                .extract().jsonPath().getString("error");
+        Response responseUser = genericMethodPostLogOutUser("", "users/logout");
 
+        int statusCode = responseUser.getStatusCode();
+        String body = responseUser.getBody().asString();
+        String errorMessage = from(body).get("error");
+
+        assertThat(statusCode, equalTo(HttpStatus.SC_UNAUTHORIZED));
         assertThat(errorMessage, equalTo("Please authenticate."));
     }
 
     /*POST Add Contact*/
     @Test
     public void addContactSuccessfullyTest(){
+        PostContactRequest contactRequest = new PostContactRequest("Rita", "Vidal", "1990-01-01", "rita@gmail.com", "8005555555",
+                "1 Main St.", "Apartment A", "Anytown", "KS", "12345", "USA");
+
         /*Login User*/
         String userToken = given()
                 .header("Content-Type", "application/json")
@@ -151,39 +136,22 @@ public class ThinkingTest {
                         "    \"email\": \"luis20@gmail.com\",\n" +
                         "    \"password\": \"myPassword\"\n" +
                         "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/login")
+                .post("users/login")
                 .then()
+                .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body("token", notNullValue())
                 .extract().jsonPath().getString("token");
 
-        given()
-                .auth()
-                .oauth2(userToken)
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"firstName\": \"Jhohny\",\n" +
-                        "    \"lastName\": \"Catch\",\n" +
-                        "    \"birthdate\": \"1970-01-01\",\n" +
-                        "    \"email\": \"jdoe@mortalKombat.com\",\n" +
-                        "    \"phone\": \"8005555555\",\n" +
-                        "    \"street1\": \"1 Main St.\",\n" +
-                        "    \"street2\": \"Apartment A\",\n" +
-                        "    \"city\": \"Anytown\",\n" +
-                        "    \"stateProvince\": \"KS\",\n" +
-                        "    \"postalCode\": \"12345\",\n" +
-                        "    \"country\": \"USA\"\n" +
-                        "}")
-                .log().all()
-                .post("https://thinking-tester-contact-list.herokuapp.com/contacts")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_CREATED);
-
+        String validateResponse =  genericMethodPostAddContact(userToken, contactRequest, "contacts", HttpStatus.SC_CREATED);
+        assertThat(validateResponse, notNullValue());
     }
 
     @Test
     public void verifyResponseOfAddContactWhenSendMandatoryFieldsEmptyTest(){
+        PostContactRequest contactRequest = new PostContactRequest("", "", "1990-01-01", "rita@gmail.com", "8005555555",
+                "1 Main St.", "Apartment A", "Anytown", "KS", "12345", "USA");
+
         /*Login User*/
         String userToken = given()
                 .header("Content-Type", "application/json")
@@ -191,39 +159,24 @@ public class ThinkingTest {
                         "    \"email\": \"luis20@gmail.com\",\n" +
                         "    \"password\": \"myPassword\"\n" +
                         "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/login")
+                .post("users/login")
                 .then()
+                .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body("token", notNullValue())
                 .extract().jsonPath().getString("token");
 
-        given()
-                .auth()
-                .oauth2(userToken)
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"firstName\": \"\",\n" +
-                        "    \"lastName\": \"\",\n" +
-                        "    \"birthdate\": \"1970-01-01\",\n" +
-                        "    \"email\": \"jdoe@mortalKombat.com\",\n" +
-                        "    \"phone\": \"8005555555\",\n" +
-                        "    \"street1\": \"1 Main St.\",\n" +
-                        "    \"street2\": \"Apartment A\",\n" +
-                        "    \"city\": \"Anytown\",\n" +
-                        "    \"stateProvince\": \"KS\",\n" +
-                        "    \"postalCode\": \"12345\",\n" +
-                        "    \"country\": \"USA\"\n" +
-                        "}")
-                .log().all()
-                .post("https://thinking-tester-contact-list.herokuapp.com/contacts")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
-
+        String validateResponse = genericMethodPostAddContact(userToken, contactRequest, "contacts", HttpStatus.SC_BAD_REQUEST);
+        String message = from(validateResponse).get("message");
+        assertThat(message, notNullValue());
+        assertThat(message, equalTo("Contact validation failed: firstName: Path `firstName` is required., lastName: Path `lastName` is required."));
     }
 
     @Test
     public void verifyResponseWhenUseInvalidTokenTest(){
+        PostContactRequest contactRequest = new PostContactRequest("Luis", "Dimitri", "1990-01-01", "rita@gmail.com", "8005555555",
+                "1 Main St.", "Apartment A", "Anytown", "KS", "12345", "USA");
+
         /*Login User*/
         String userToken = given()
                 .header("Content-Type", "application/json")
@@ -231,38 +184,24 @@ public class ThinkingTest {
                         "    \"email\": \"luis20@gmail.com\",\n" +
                         "    \"password\": \"myPassword\"\n" +
                         "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/login")
+                .post("users/login")
                 .then()
+                .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body("token", notNullValue())
                 .extract().jsonPath().getString("token");
 
-        given()
-                .auth()
-                .oauth2(userToken+"a")
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"firstName\": \"Java\",\n" +
-                        "    \"lastName\": \"Du\",\n" +
-                        "    \"birthdate\": \"1970-01-01\",\n" +
-                        "    \"email\": \"jdoe@mortalKombat.com\",\n" +
-                        "    \"phone\": \"8005555555\",\n" +
-                        "    \"street1\": \"1 Main St.\",\n" +
-                        "    \"street2\": \"Apartment A\",\n" +
-                        "    \"city\": \"Anytown\",\n" +
-                        "    \"stateProvince\": \"KS\",\n" +
-                        "    \"postalCode\": \"12345\",\n" +
-                        "    \"country\": \"USA\"\n" +
-                        "}")
-                .log().all()
-                .post("https://thinking-tester-contact-list.herokuapp.com/contacts")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED);
+        String validateResponse = genericMethodPostAddContact(userToken+"a", contactRequest, "contacts", HttpStatus.SC_UNAUTHORIZED);
+        String errorMessage = from(validateResponse).get("error");
+        assertThat(errorMessage, notNullValue());
+        assertThat(errorMessage, equalTo("Please authenticate."));
     }
 
     @Test
     public void getResponseWhenAddContactSuccessfullyTest(){
+        PostContactRequest contactRequest = new PostContactRequest("Java java", "Du", "1990-01-01", "rita@gmail.com", "8005555555",
+                "1 Main St.", "Apartment A", "Anytown", "KS", "12345", "USA");
+
         /*Login User*/
         String userToken = given()
                 .header("Content-Type", "application/json")
@@ -270,37 +209,52 @@ public class ThinkingTest {
                         "    \"email\": \"luis20@gmail.com\",\n" +
                         "    \"password\": \"myPassword\"\n" +
                         "}")
-                .post("https://thinking-tester-contact-list.herokuapp.com/users/login")
+                .post("users/login")
                 .then()
+                .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body("token", notNullValue())
                 .extract().jsonPath().getString("token");
 
-        String contactBirthdate = given()
-                .auth()
-                .oauth2(userToken)
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"firstName\": \"Java\",\n" +
-                        "    \"lastName\": \"Du\",\n" +
-                        "    \"birthdate\": \"1970-01-01\",\n" +
-                        "    \"email\": \"jdoe@mortalKombat.com\",\n" +
-                        "    \"phone\": \"8005555555\",\n" +
-                        "    \"street1\": \"1 Main St.\",\n" +
-                        "    \"street2\": \"Apartment A\",\n" +
-                        "    \"city\": \"Anytown\",\n" +
-                        "    \"stateProvince\": \"KS\",\n" +
-                        "    \"postalCode\": \"12345\",\n" +
-                        "    \"country\": \"USA\"\n" +
-                        "}")
-                .log().all()
-                .post("https://thinking-tester-contact-list.herokuapp.com/contacts")
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_CREATED)
-                .body("_id", notNullValue())
-                .extract().jsonPath().getString("birthdate");
+        String validateResponse = genericMethodPostAddContact(userToken, contactRequest, "contacts", HttpStatus.SC_CREATED);
 
-        assertThat(contactBirthdate, equalTo("1970-01-01"));
+        String contactId = from(validateResponse).get("_id");
+        assertThat(contactId, notNullValue());
+        String contactBirthdate = from(validateResponse).get("birthdate");
+        assertThat(contactBirthdate, equalTo("1990-01-01"));
+    }
+
+    /*Refactor Method*/
+    //AddUser
+    private Response genericMethodPostAddUser(PostUserRequest userRequest, String path) {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(userRequest)
+                .post(path);
+        return response;
+    }
+
+    //Log out User
+    private Response genericMethodPostLogOutUser(String token, String path) {
+        Response response = given()
+                .auth()
+                .oauth2(token)
+                .post(path);
+        return response;
+    }
+
+    //Contact
+    private String genericMethodPostAddContact(String token, PostContactRequest contactRequest, String request, int codeStatus) {
+        String response =  given()
+                .auth()
+                .oauth2(token)
+                .contentType(ContentType.JSON)
+                .body(contactRequest)
+                .post(request)
+                .then()
+                .assertThat()
+                .statusCode(codeStatus)
+                .extract().body().asString();
+        return response;
     }
 }
